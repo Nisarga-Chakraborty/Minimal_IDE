2cd0836a6d75410bad853c918adc96fe6b61384e
Nisarga Chakraborty <nisargac@op.iitg.ac.in>
2025-12-10 22:34:38 +0530

    Implement MyApi class with API key and URL methods

---
+++ MyApi.java
@@ -0,0 +1,12 @@
+public class MyApi 
+{
+    public String apikey()
+    {
+        String apikey="...."; // MY API KEY
+        return apikey;
+    }
+    
+    public String apiUrl() {
+    return "...";// API URL
+}
+}
